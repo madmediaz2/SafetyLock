e4d4d4435f8048dfd63d0cf340b02f9dc1136445
madmediaz2 <simar.cakmak@gmail.com>
2024-04-05 12:18:01 +0200

    Added functionality to save the information in an interface "Credential"

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,29 @@
-console.log("Hello, World!");
+document.addEventListener("DOMContentLoaded", function (event) {
+    console.log("Hello, World!");
+    var usernameElement = document.getElementById("script--credential-username");
+    var showButton = document.getElementById("script--button-show");
+    var urlElement = document.getElementById("script--credential-URL");
+    var passwordElement = document.getElementById("script--credential-pw");
+    var appendButton = document.getElementById("script--button-append");
+    var footerButton = document.getElementById("script--button-footer");
+    if (appendButton) {
+        appendButton.addEventListener("click", function () {
+            if (!urlElement || !usernameElement || !passwordElement) {
+                console.log("One or more elements are missing, returning out of function");
+                return;
+            }
+            var url = urlElement.value;
+            var username = usernameElement.value;
+            var password = passwordElement.value;
+            var hidden = true;
+            var credential = {
+                id: Date.now().toString(),
+                url: url,
+                username: username,
+                password: password,
+                hidden: hidden,
+            };
+            console.log(credential);
+        });
+    }
+});
--- src/js/main.ts
+++ src/js/main.ts
@@ -1,8 +1,55 @@
-console.log("Hello, World!");
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("Hello, World!");
 
-interface credentials {
-    id: number;
-    username: string;
-    password: string;
-    hidden: boolean;
-}
+    interface Credentials {
+        id: string;
+        url: string;
+        username: string;
+        password: string;
+        hidden: boolean;
+    }
+
+    const usernameElement = document.getElementById(
+        "script--credential-username"
+    ) as HTMLInputElement | null;
+    const showButton = document.getElementById(
+        "script--button-show"
+    ) as HTMLButtonElement | null;
+    const urlElement = document.getElementById(
+        "script--credential-URL"
+    ) as HTMLInputElement | null;
+    const passwordElement = document.getElementById(
+        "script--credential-pw"
+    ) as HTMLInputElement | null;
+    const appendButton = document.getElementById(
+        "script--button-append"
+    ) as HTMLButtonElement | null;
+    const footerButton = document.getElementById(
+        "script--button-footer"
+    ) as HTMLButtonElement | null;
+
+    if (appendButton) {
+        appendButton.addEventListener("click", () => {
+            if (!urlElement || !usernameElement || !passwordElement) {
+                console.log(
+                    "One or more elements are missing, returning out of function"
+                );
+                return;
+            }
+            let url: string = urlElement.value;
+            let username: string = usernameElement.value;
+            let password: string = passwordElement.value;
+            let hidden = true;
+
+            const credential: Credentials = {
+                id: Date.now().toString(),
+                url: url,
+                username: username,
+                password: password,
+                hidden: hidden,
+            };
+
+            console.log(credential);
+        });
+    }
+});
